7bb4ead208bf1e72dee7a0f42dc88faa862b531a
Nilvaa <avlinantony26@gmail.com>
2026-08-14 11:41:06 +0530

    database url  fix

--- backend/database.py
+++ backend/database.py
@@ -5,10 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 def get_connection():
     connection=psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
+        os.getenv("DATABASE_URL")
     )
     return connection
